c8850af20b6b5dd93cb0ec332596b50dc23557ed
Kelvin Agbe <139677635+Kelvinagbe@users.noreply.github.com>
2025-05-05 03:30:34 +0100

    Create games-main.js

---
+++ js/lib/games-main.js
@@ -0,0 +1,28 @@
+
+// Games for index page
+const games = [
+  {
+    id: "truth",
+    title: "Truth or Dare",
+    rating: "4.8",
+    players: "12k+",
+    image: "../assets/image/truth.jpg",
+    new: true
+  },
+  {
+    id: "pixel-shooter",
+    title: "Pixel Shooter",
+    rating: "4.6",
+    players: "8k+",
+    image: "../assets/images/pixel-shooter.jpg",
+    new: false
+  },
+  {
+    id: "subway",
+    title: "Subway Surfers Helloween",
+    rating: "4.9",
+    players: "20k+",
+    image: "../assest/images/subway.jpg",
+    new: true
+  }
+];
