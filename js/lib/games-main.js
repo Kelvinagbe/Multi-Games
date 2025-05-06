a814e0fe3b27992cfd792e78570d41b3c9b85932
Kelvin Agbe <139677635+Kelvinagbe@users.noreply.github.com>
2025-05-06 12:10:04 +0100

    Update games-main.js

--- js/lib/games-main.js
+++ js/lib/games-main.js
@@ -1,28 +1,219 @@
 
 // Games for index page
+// Games for index page
 const games = [
+  // Featured Game
   {
-    id: "truth",
+    id: "01",
     title: "Truth or Dare",
+    rating: "4.9",
+    players: "20k+",
+    image: "../assets/image/01.jpg",
+    new: false
+  },
+  
+  // Popular Games
+  {
+    id: "02",
+    title: "Games james",
     rating: "4.8",
-    players: "12k+",
-    image: "../assets/image/truth.jpg",
+    players: "10k+",
+    image: "../assets/image/02.jpg",
     new: true
   },
   {
-    id: "pixel-shooter",
-    title: "Pixel Shooter",
-    rating: "4.6",
+    id: "03",
+    title: "Whack Mole",
+    rating: "4.5",
     players: "8k+",
-    image: "../assets/image/pixel-shooter.jpg",
+    image: "../assets/image/03.jpg",
+    new: true
+  },
+  {
+    id: "04",
+    title: "Tic Tac Toe",
+    rating: "4.7",
+    players: "15k+",
+    image: "../assets/image/04.jpg",
+    new: false
+  },
+  
+  // New Releases
+  {
+    id: "05",
+    title: "Bubble Shoot",
+    rating: "4.3",
+    players: "5k+",
+    image: "../assets/image/05.jpg",
+    new: true
+  },
+  {
+    id: "06",
+    title: "Macthman Warrior",
+    rating: "4.6",
+    players: "3k+",
+    image: "../assets/image/06.jpg",
+    new: true
+  },
+  {
+    id: "07",
+    title: "Moto X3M Christmas",
+    rating: "4.6",
+    players: "6k+",
+    image: "../assets/image/07.png",
+    new: true
+  },
+  {
+    id: "08",
+    title: "Ludo Legend",
+    rating: "4.6",
+    players: "6k+",
+    image: "../assets/image/08.png",
+    new: true
+  },
+  {
+    id: "09",
+    title: "Bike Rush 3D",
+    rating: "4.6",
+    players: "6k+",
+    image: "../assets/image/09.jpg",
+    new: true
+  },
+  {
+    id: "10",
+    title: "Boat Race",
+    rating: "4.6",
+    players: "6k+",
+    image: "../assets/image/10.png",
+    new: true
+  },
+  
+  // Trending and Popular Games
+  {
+    id: "11",
+    title: "Subway Surfers Halloween",
+    rating: "4.8",
+    players: "10k+",
+    image: "../assets/image/11.jpg",
+    new: true
+  },
+  {
+    id: "12",
+    title: "Pubg Mobile",
+    rating: "4.5",
+    players: "8k+",
+    image: "../assets/image/12.jpg",
     new: false
   },
   {
-    id: "subway",
-    title: "Subway Surfers Helloween",
+    id: "13",
+    title: "Pixel Shooter",
+    rating: "4.5",
+    players: "8k+",
+    image: "../assets/image/13.jpg",
+    new: false
+  },
+  {
+    id: "14",
+    title: "Relic Run Away",
+    rating: "4.5",
+    players: "8k+",
+    image: "../assets/image/14.jpg",
+    new: false
+  },
+  {
+    id: "15",
+    title: "Hunter Assassin",
     rating: "4.9",
-    players: "20k+",
-    image: "../assets/image/subway.jpg",
-    new: true
+    players: "25k+",
+    image: "../assets/image/15.jpg",
+    new: true,
+    hot: true
+  },
+  {
+    id: "16",
+    title: "Squid Survival",
+    rating: "4.9",
+    players: "25k+",
+    image: "../assets/image/16.png",
+    new: false,
+    hot: true
+  },
+  {
+    id: "17",
+    title: "Piano Music Tiles",
+    rating: "4.9",
+    players: "25k+",
+    image: "../assets/image/17.webp",
+    new: false,
+    hot: true
+  },
+  {
+    id: "18",
+    title: "Sniper Simulator",
+    rating: "1.8k", // Note: This seems to show rating count rather than score
+    players: "25k+",
+    image: "../assets/image/18.jpg",
+    new: false,
+    hot: true
+  },
+  {
+    id: "19",
+    title: "Wild Hunt Hunting",
+    rating: "1.8k",
+    players: "25k+",
+    image: "../assets/image/19.png",
+    new: false,
+    hot: true
+  },
+  {
+    id: "20",
+    title: "Egg Super Adventure",
+    rating: "1.0k",
+    players: "2k+",
+    image: "../assets/image/20.jpg",
+    new: false,
+    hot: true
+  },
+  {
+    id: "21",
+    title: "Rac Simulator 1",
+    rating: "1.8k",
+    players: "25k+",
+    image: "../assets/image/21.png",
+    new: false,
+    hot: true
+  },
+  {
+    id: "22",
+    title: "Smash Colors Music Ball",
+    rating: "1.0k",
+    players: "2k+",
+    image: "../assets/image/22.png",
+    new: false,
+    hot: true
+  },
+  {
+    id: "23",
+    title: "Subway Surfers Holiday",
+    rating: "1.8k",
+    players: "25k+",
+    image: "../assets/image/23.png",
+    new: false,
+    hot: true
+  },
+  {
+    id: "24",
+    title: "ORV Racing",
+    rating: "1.0k",
+    players: "2k+",
+    image: "../assets/image/24.png",
+    new: false,
+    hot: true
   }
 ];
+
+// You can now use this games array in your application
+// Example usage:
+// const featuredGames = games.filter(game => game.new);
+// const hotGames = games.filter(game => game.hot);
